3a02eccd6f7aaa4506de050a783431703d443d5d
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-12-20 22:08:14 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/test.sh
@@ -0,0 +1,2 @@
+javac -cp "." implementation/main.java implementation/TicTacToe.java implementation/TicTacToeTrain.java implementation/XOButton.java implementation/Reinforcement.java
+java -cp "." implementation.Main
