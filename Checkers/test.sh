954c46efa70af4c976861fb7cfce0b499abc9dca
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-12-25 19:04:27 +0530

    Add files via upload

---
+++ Checkers/test.sh
@@ -0,0 +1,2 @@
+javac -cp "." implementation/Board.java implementation/Checker.java implementation/CheckerType.java implementation/Checkers.java implementation/AlreadyOccupiedException.java
+java -cp "." implementation.Checkers
